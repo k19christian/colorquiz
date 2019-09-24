cfde5ca968a8e6a1cbd9d686535ee3f72e1ba756
Christian Immanuel Kurniawan <55701124+k19christian@users.noreply.github.com>
2019-09-24 23:56:27 +0700

    Commit!
    Commit

---
+++ js/question.js
@@ -0,0 +1,467 @@
+
+
+var question2 = new Array(3);
+
+for (var i = 0; i < 3; i++) {
+	question2[i] = new Array(5);
+}
+
+question2[0][0] = {
+	"question" : "What color is this ?",
+	"color" : "#000080",
+	"option1" : "Navy",
+	"option2" : "Dark Blue",
+	"option3" : "Blue",
+	"option4" : "Black",
+	"answer"  : "1"	
+};
+
+question2[0][1] = {
+	"question" : "What color is this ?",
+	"color" : "#4B0082",
+	"option1" : "Grey",
+	"option2" : "Indigo",
+	"option3" : "Blue",
+	"option4" : "Green",
+	"answer"  : "2"		
+};
+
+question2[0][2] = {
+	"question" : "What color is this ?",
+	"color" : "#FFD700",
+	"option1" : "Gold",
+	"option2" : "Blue",
+	"option3" : "Green",
+	"option4" : "White",
+	"answer"  : "1"	
+};
+
+question2[0][3] = {
+	"question" : "What color is this ?",
+	"color" : "	#00FF00",
+	"option1" : "Green",
+	"option2" : "Light Green",
+	"option3" : "Chocolate",
+	"option4" : "Lime",
+	"answer": "4"	
+};
+
+question2[0][4] = {
+	"question" : "What color is this ?",
+	"color" : "#7CFC00",
+	"option1" : "Blue",
+	"option2" : "Tosca",
+	"option3" : "Lawn Green",
+	"option4" : "Sea Green",
+	"answer": "3"	
+};
+
+question2[1][0] = {
+	"question" : "What color is this ?",
+	"color" : "#800000",
+	"option1" : "Blue",
+	"option2" : "Maroon",
+	"option3" : "White",
+	"option4" : "Green",
+	"answer"  : "2"	
+};
+
+question2[1][1] = {
+	"question" : "What color is this ?",
+	"color" : "#90EE90",
+	"option1" : "Blue",
+	"option2" : "Black",
+	"option3" : "Light Green",
+	"option4" : "Dark White",
+	"answer": "3"		
+};
+
+question2[1][2] = {
+	"question" : "What color is this ?",
+	"color" : "#7FFFD4",
+	"option1" : "Aqua Marine",
+	"option2" : "White",
+	"option3" : "Purple",
+	"option4" : "Blue",
+	"answer"  : "1"
+};
+
+question2[1][3] = {
+	"question" : "What color is this ?",
+	"color" : "#8B008B",
+	"option1" : "Green",
+	"option2" : "Yellow",
+	"option3" : "Red",
+	"option4" : "Dark Magenta",
+	"answer"  : "4"	
+};
+
+question2[1][4] = {
+	"question" : "What color is this ?",
+	"color" : "#D2B48C",
+	"option1" : "Grey",
+	"option2" : "Tan",
+	"option3" : "Brown",
+	"option4" : "Green",
+	"answer"  : "2"
+};
+
+question2[2][0] = {
+	"question" : "What color is this ?",
+	"color" : "#DC143C",
+	"option1" : "Indigo",
+	"option2" : "Red",
+	"option3" : "Maroon",
+	"option4" : "Crimson",
+	"answer"  : "4"	
+};
+
+question2[2][1] = {
+	"question" : "What color is this ?",
+	"color" : "#4B0082",
+	"option1" : "Grey",
+	"option2" : "Indigo",
+	"option3" : "Blue",
+	"option4" : "Green",
+	"answer"  : "2"		
+};
+
+question2[2][2] = {
+	"question" : "What color is this ?",
+	"color" : "#F5F5F5",
+	"option1" : "Ghost White",
+	"option2" : "White Plain",
+	"option3" : "White Smoke",
+	"option4" : "Beige",
+	"answer": "3"
+};
+
+question2[2][3] = {
+	"question" : "What color is this ?",
+	"color" : "#00FF7F",
+	"option1" : "Spring Green",
+	"option2" : "Light Green",
+	"option3" : "Lawn Green",
+	"option4" : "Sea Green",
+	"answer": "1"
+};
+
+question2[2][4] = {
+	"question" : "What color is this ?",
+	"color" : "	#228B22",
+	"option1" : "Green",
+	"option2" : "Light Green",
+	"option3" : "Lawn Green",
+	"option4" : "Forest Green",
+	"answer": "4"
+};
+var question = new Array(3);
+
+for (var i = 0; i < 3; i++) {
+	question[i] = new Array(5);
+}
+
+question[0][0] = {
+	"question" : "What color is this ?",
+	"color" : "#138D75",
+	"option1" : "Green",
+	"option2" : "Blue",
+	"option3" : "Yellow",
+	"option4" : "Black",
+	"answer"  : "1"	
+};
+
+question[0][1] = {
+	"question" : "What color is this ?",
+	"color" : "#FDFEFE",
+	"option1" : "White",
+	"option2" : "Red",
+	"option3" : "Blue",
+	"option4" : "Green",
+	"answer"  : "1"		
+};
+
+question[0][2] = {
+	"question" : "What color is this ?",
+	"color" : "#17202A",
+	"option1" : "White",
+	"option2" : "Blue",
+	"option3" : "Green",
+	"option4" : "Black",
+	"answer"  : "4"	
+};
+
+question[0][3] = {
+	"question" : "What color is this ?",
+	"color" : "	#FFEE58",
+	"option1" : "Green",
+	"option2" : "Brown",
+	"option3" : "Chocolate",
+	"option4" : "Yellow",
+	"answer": "4"	
+};
+
+question[0][4] = {
+	"question" : "What color is this ?",
+	"color" : "#607D8B",
+	"option1" : "Blue",
+	"option2" : "Red",
+	"option3" : "Grey",
+	"option4" : "Green",
+	"answer": "3"	
+};
+
+question[1][0] = {
+	"question" : "What color is this ?",
+	"color" : "#F5F5DC",
+	"option1" : "Beige",
+	"option2" : "Light Beige",
+	"option3" : "Purple",
+	"option4" : "Blue",
+	"answer"  : "1"
+};
+
+question[1][1] = {
+	"question" : "What color is this ?",
+	"color" : "#40E0D0",
+	"option1" : "Blue",
+	"option2" : "Black",
+	"option3" : "Turquoise",
+	"option4" : "Dark White",
+	"answer": "3"		
+};
+
+question[1][2] = {
+	"question" : "What color is this ?",
+	"color" : "#000000",
+	"option1" : "Black",
+	"option2" : "White",
+	"option3" : "Purple",
+	"option4" : "Blue",
+	"answer"  : "1"
+};
+
+question[1][3] = {
+	"question" : "What color is this ?",
+	"color" : "#00FFFF",
+	"option1" : "Green",
+	"option2" : "Yellow",
+	"option3" : "Red",
+	"option4" : "Cyan",
+	"answer"  : "4"	
+};
+
+question[1][4] = {
+	"question" : "What color is this ?",
+	"color" : "#800080",
+	"option1" : "Grey",
+	"option2" : "Purple",
+	"option3" : "Brown",
+	"option4" : "Green",
+	"answer"  : "2"
+};
+
+question[2][0] = {
+	"question" : "What color is this ?",
+	"color" : "#003300",
+	"option1" : "Light Green",
+	"option2" : "Red",
+	"option3" : "Light Grey",
+	"option4" : "Dark Green",
+	"answer"  : "4"	
+};
+
+question[2][1] = {
+	"question" : "What color is this ?",
+	"color" : "#0099FF",
+	"option1" : "Grey",
+	"option2" : "Light Blue",
+	"option3" : "Blue",
+	"option4" : "Green",
+	"answer"  : "2"		
+};
+
+question[2][2] = {
+	"question" : "What color is this ?",
+	"color" : "#663300",
+	"option1" : "White",
+	"option2" : "Black",
+	"option3" : "Brown",
+	"option4" : "Beige",
+	"answer": "3"
+};
+
+question[2][3] = {
+	"question" : "What color is this ?",
+	"color" : "#00FF7F",
+	"option1" : "Spring Green",
+	"option2" : "Brown",
+	"option3" : "Blue",
+	"option4" : "Yellow",
+	"answer": "1"
+};
+
+question[2][4] = {
+	"question" : "What color is this ?",
+	"color" : "	#228B22",
+	"option1" : "Blue",
+	"option2" : "Orange",
+	"option3" : "Red",
+	"option4" : "Forest Green",
+	"answer": "4"
+};
+var question3 = new Array(3);
+
+for (var i = 0; i < 3; i++) {
+	question3[i] = new Array(5);
+}
+
+question3[0][0] = {
+	"question" : "What color is this ?",
+	"color" : "#00FA9A",
+	"option1" : "Green",
+	"option2" : "Medium Spring Green",
+	"option3" : "Light Spring Green",
+	"option4" : "Spring Green",
+	"answer"  : "2"	
+};
+
+question3[0][1] = {
+	"question" : "What color is this ?",
+	"color" : "#00FFFF",
+	"option1" : "Blue",
+	"option2" : "Light Blue",
+	"option3" : "Aqua",
+	"option4" : "Water Blue",
+	"answer"  : "3"		
+};
+
+question3[0][2] = {
+	"question" : "What color is this ?",
+	"color" : "#5F9EA0",
+	"option1" : "Cadet Blue",
+	"option2" : "Steel Blue",
+	"option3" : "Sergeant Blue",
+	"option4" : "Blue",
+	"answer"  : "1"	
+};
+
+question3[0][3] = {
+	"question" : "What color is this ?",
+	"color" : "	##00BFFF",
+	"option1" : "Deep Sky Blue",
+	"option2" : "Light Sky Blue",
+	"option3" : "Sky Blue",
+	"option4" : "Yellow",
+	"answer": "1"	
+};
+
+question3[0][4] = {
+	"question" : "What color is this ?",
+	"color" : "#607D8B",
+	"option1" : "Blue",
+	"option2" : "Red",
+	"option3" : "Grey",
+	"option4" : "Green",
+	"answer": "3"	
+};
+
+question3[1][0] = {
+	"question" : "What color is this ?",
+	"color" : "#191970",
+	"option1" : "Blue",
+	"option2" : "Sunrise Blue",
+	"option3" : "Midnight Blue",
+	"option4" : "Afternoon Blue",
+	"answer"  : "3"	
+};
+
+question3[1][1] = {
+	"question" : "What color is this ?",
+	"color" : "#EE82EE",
+	"option1" : "Violet",
+	"option2" : "Pink",
+	"option3" : "Light Pink",
+	"option4" : "Dark Pink",
+	"answer": "1"		
+};
+
+question3[1][2] = {
+	"question" : "What color is this ?",
+	"color" : "#F5F5DC",
+	"option1" : "Beige",
+	"option2" : "Light Beige",
+	"option3" : "Purple",
+	"option4" : "Blue",
+	"answer"  : "1"
+};
+
+question3[1][3] = {
+	"question" : "What color is this ?",
+	"color" : "#F5DEB3",
+	"option1" : "Milk",
+	"option2" : "Cream",
+	"option3" : "Wheat",
+	"option4" : "White + Brown",
+	"answer"  : "3"	
+};
+
+question3[1][4] = {
+	"question" : "What color is this ?",
+	"color" : "#A0522D",
+	"option1" : "Red",
+	"option2" : "Sienna",
+	"option3" : "Red Light",
+	"option4" : "brown Light",
+	"answer"  : "2"
+};
+
+question3[2][0] = {
+	"question" : "What color is this ?",
+	"color" : "#FFF0F5",
+	"option1" : "Lavender Blush",
+	"option2" : "Pink",
+	"option3" : "Light Pink",
+	"option4" : "Dark Green",
+	"answer"  : "1"	
+};
+
+question3[2][1] = {
+	"question" : "What color is this ?",
+	"color" : "#696969",
+	"option1" : "Grey",
+	"option2" : "Light Grey",
+	"option3" : "Dim Grey",
+	"option4" : "Dark Grey",
+	"answer"  : "3"		
+};
+
+question3[2][2] = {
+	"question" : "What color is this ?",
+	"color" : "#DCDCDC",
+	"option1" : "Gainsboro",
+	"option2" : "Orchid",
+	"option3" : "Thistle",
+	"option4" : "Beige",
+	"answer": "1"
+};
+
+question3[2][3] = {
+	"question" : "What color is this ?",
+	"color" : "#228B22",
+	"option1" : "Spring Green",
+	"option2" : "Dark Green",
+	"option3" : "Green",
+	"option4" : "Forest Green",
+	"answer": "4"
+};
+
+question3[2][4] = {
+	"question" : "What color is this ?",
+	"color" : "	#ADD8E6",
+	"option1" : "Light Blue",
+	"option2" : "Blue",
+	"option3" : "Red",
+	"option4" : "Forest Green",
+	"answer": "1"
+};
---
+++ js/quiz-script-3.js
@@ -0,0 +1,98 @@
+var currentQuestion = 0;
+var score = 0;
+var x = getRndInteger(0,2);
+var totQuestions = question3[x].length;
+
+
+var container = document.getElementById('quizContainer');
+var questionEl = document.getElementById('question');
+var col = document.getElementById('color')
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
+var redoButton = document.getElementById('redoButton');
+var resultBox = document.getElementById('box');
+
+
+
+function loadQuestion (questionIndex) {
+	var q = question3[x][questionIndex];
+		questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
+		col.style.backgroundColor = q.color;
+		opt1.textContent = q.option1;
+		opt2.textContent = q.option2;
+		opt3.textContent = q.option3;
+		opt4.textContent = q.option4;
+};
+
+function getRndInteger(min,max) {
+	return Math.floor(Math.random() * (max-min + 1)) + min;
+};
+
+function loadNextQuestion () {
+	var selectedOption = document.querySelector('input[type=radio]:checked');
+	if(!selectedOption){
+		alert('Please select your answer !');
+		return;
+	}
+	var answer = selectedOption.value;
+	if(question3[x][currentQuestion].answer != answer){
+		alert('Right ? WRONG !');	
+	}
+	if(question3[x][currentQuestion].answer == answer){
+		alert('Correct !');	
+		score += 20;
+	}
+	selectedOption.checked = false;
+	currentQuestion++;
+	if(currentQuestion == totQuestions - 1){
+		nextButton.textContent = 'Finish';
+	}
+	if(currentQuestion == totQuestions){
+		resultCont.setAttribute('style', 'white-space: pre;');
+		container.style.display = 'none';
+		resultCont.style.display = '';
+		resultBox.style.display = '';
+		resultCont.textContent = 'Your Score: ' + score + " / 100 \r\n";
+		if (score == 0) {
+			resultCont.textContent += "\r\nWOAH, BAD LUCK !";
+			redoButton.style.display = '';
+			redoButton.textContent = 'Try Again ?';
+		}
+		if (score == 20) {
+			resultCont.textContent += "\r\nBETTER THAN NOTHING RIGHT ?";
+			redoButton.style.display = '';
+			redoButton.textContent = 'Try Again ?';
+		}
+		if (score == 40) {
+			resultCont.textContent += "\r\nARE YOU DUMB OR SOMETHING ?";
+			redoButton.style.display = '';
+			redoButton.textContent = 'Try Again ?';
+		}
+		if (score == 60) {
+			resultCont.textContent += "\r\nA SCORE FOR RETAKE ?!";
+			redoButton.style.display = '';
+			redoButton.textContent = 'Try Again ?';
+		}
+		if (score == 80) {
+			resultCont.textContent += "\r\nNICELY DONE !";
+			redoButton.style.display = '';
+			redoButton.textContent = 'Try Again ?';
+		}
+		if (score == 100) {
+			resultCont.textContent += "\r\nY O U  A R E  A W E S O M E !";
+			redoButton.style.display = '';
+			redoButton.textContent = 'Try Again ?';
+		}
+		return;
+	}
+	loadQuestion(currentQuestion);
+}
+function retakeQuiz () {
+	window.location.href="index3.html"
+};
+
+loadQuestion(currentQuestion);
